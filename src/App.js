408108f21d7f9cfdfbfc59d109c32c7b73f3a9fe
wanzulfikri <wanzulfikri@gmail.com>
2020-01-27 19:38:29 +0800

    Add stub fetchRandomMovie function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
 import "./App.css";
+import fetchRandomMovie from "./api/fetchMovie";
+
+async function handleClick(e) {
+  const movieRequest = await fetchRandomMovie();
+  console.log(movieRequest);
+}
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Button onClick={handleClick} variant="contained" color="primary">
+        Random Movie
+      </Button>
+    </div>
+  );
 }
 
 export default App;
