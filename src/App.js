e69b8cb9e45ba98968037540654e3770485c4a87
wanzulfikri <wanzulfikri@gmail.com>
2020-01-27 20:38:38 +0800

    Add grid for randomise button and movie title

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 import "./App.css";
 import fetchRandomMovie from "./api/fetchMovie";
 
-async function handleClick(e) {
-  const movieRequest = await fetchRandomMovie();
-  console.log(movieRequest);
+function handleClick(setState) {
+  const promise = fetchRandomMovie();
+  const fetchedMovie = promise
+    .then(function(response) {
+      setState(response.data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
 }
 
 function App() {
+  const [movie, setMovie] = useState(null);
+  // const { title } = movie;
   return (
     <div className="App">
-      <Button onClick={handleClick} variant="contained" color="primary">
-        Random Movie
-      </Button>
+      <Container maxWidth="md">
+        <Card>
+          <CardContent>
+            <Grid container justify="center">
+              {movie ? movie.Title : "None"}
+            </Grid>
+            <Grid container justify="center">
+              <Button
+                onClick={() => handleClick(setMovie)}
+                variant="contained"
+                color="primary"
+              >
+                Random Movie
+              </Button>
+            </Grid>
+          </CardContent>
+        </Card>
+      </Container>
     </div>
   );
 }
--- src/api/fetchMovie.js
+++ src/api/fetchMovie.js
@@ -1,15 +1,8 @@
 import axios from "axios";
 
 // WIP
-async function fetchRandomMovie() {
-  return await axios
-    .get("http://www.omdbapi.com/?apikey=819fc38b&i=tt0000001")
-    .then(function(response) {
-      return response.data;
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
+function fetchRandomMovie() {
+  return axios.get("http://www.omdbapi.com/?apikey=819fc38b&i=tt0000001");
 }
 
 export default fetchRandomMovie;
